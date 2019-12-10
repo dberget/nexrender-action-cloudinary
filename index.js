2fff47cb4621950730877a45e89c1abfb95ffeb8
davidberget@gmail.com <davidberget@gmail.com>
2019-12-10 15:28:51 -0600

    streaming

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ module.exports = (
         public_id: `videos/${job.uid}`,
         overwrite: true,
         notification_url: "",
-        eager: [{ quality: "50" }]
+        eager: [{ streaming_profile: "hd", format: "m3u8", quality: "50" }]
       },
       function(error, result) {
         console.log(result, error);
