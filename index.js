8f2bbc29fd276770f64f681c5ed297c7f71c67c0
davidberget@gmail.com <davidberget@gmail.com>
2019-12-10 12:34:56 -0600

    add catch

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ module.exports = (
   { input, api_key, cloud_name, api_secret },
   type
 ) => {
+  cloudinary.config({
+    cloud_name: cloud_name,
+    api_key: api_key,
+    api_secret: api_secret
+  });
+
+  input = input || job.output;
+
+  if (!path.isAbsolute(input)) input = path.join(job.workpath, input);
+
   return new Promise((resolve, reject) => {
-    cloudinary.config({
-      cloud_name: cloud_name,
-      api_key: api_key,
-      api_secret: api_secret
-    });
-
-    input = input || job.output;
-
-    if (!path.isAbsolute(input)) input = path.join(job.workpath, input);
-
     cloudinary.uploader.upload(
       input,
       {
@@ -33,5 +33,7 @@ module.exports = (
         resolve(job);
       }
     );
+  }).catch(error => {
+    throw error;
   });
 };
